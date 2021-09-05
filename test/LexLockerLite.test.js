156ed08f68d85b8b1f20c4f2dcee8e9b01ca6139
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-09-04 21:21:29 -0400

    Update LexLockerLite.test.js
    nit to make latest hardhat happy

--- test/LexLockerLite.test.js
+++ test/LexLockerLite.test.js
@@ -7,7 +7,7 @@ const bentoAddress = "0xF5BCE5077908a1b7370B9ae04AdC565EBd643966";
 chai
   .use(solidity)
   .should();
-
+ 
 describe("LexLockerLite", function () {
   it("Should take an ERC20 token deposit and allow release by depositor", async function () {
     let depositor, receiver, resolver;
@@ -16,10 +16,6 @@ describe("LexLockerLite", function () {
     const Token = await ethers.getContractFactory("TestERC20");
     const token = await Token.deploy("poc", "poc", 100000);
     await token.deployed();
- 
-    const NFT = await ethers.getContractFactory("TestERC721");
-    const nft = await NFT.deploy();
-    await nft.deployed();
  
     const Locker = await ethers.getContractFactory("LexLockerLite");
     const locker = await Locker.deploy();
@@ -34,7 +30,7 @@ describe("LexLockerLite", function () {
     await locker.deposit(receiver.address, resolver.address, token.address, 1000, false, "TEST");
     await locker.release(1);
   });
-
+ 
   it("Should take an ERC20 token deposit and forbid release by non-depositor", async function () {
     let depositor, receiver, resolver;
     [depositor, receiver, resolver] = await ethers.getSigners();
@@ -56,7 +52,7 @@ describe("LexLockerLite", function () {
     await locker.deposit(receiver.address, resolver.address, token.address, 1000, false, "TEST");
     await locker.connect(receiver).release(1).should.be.revertedWith("not depositor");
   });
-
+ 
   it("Should take an ERC721 NFT deposit and allow release by depositor", async function () {
     let depositor, receiver, resolver;
     [depositor, receiver, resolver] = await ethers.getSigners();
@@ -68,7 +64,7 @@ describe("LexLockerLite", function () {
     const Locker = await ethers.getContractFactory("LexLockerLite");
     const locker = await Locker.deploy();
     await locker.deployed();
-
+ 
     await nft.mint(1, "TEST");
     await nft.approve(locker.address, 1);
     
@@ -76,7 +72,7 @@ describe("LexLockerLite", function () {
     await locker.deposit(receiver.address, resolver.address, nft.address, 1, true, "TEST");
     await locker.release(1);
   });
-
+ 
   it("Should take an ERC721 NFT deposit and forbid release by non-depositor", async function () {
     let depositor, receiver, resolver;
     [depositor, receiver, resolver] = await ethers.getSigners();
@@ -88,7 +84,7 @@ describe("LexLockerLite", function () {
     const Locker = await ethers.getContractFactory("LexLockerLite");
     const locker = await Locker.deploy();
     await locker.deployed();
-
+ 
     await nft.mint(1, "TEST");
     await nft.approve(locker.address, 1);
     
@@ -97,3 +93,5 @@ describe("LexLockerLite", function () {
     await locker.connect(receiver).release(1).should.be.revertedWith("not depositor");
   });
 });
+ 
+
