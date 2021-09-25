ba0c7b2a68d77f51eed2cf9588c318ca40077346
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-09-24 20:34:43 -0400

    Update TestERC721.sol

--- contracts/mocks/TestERC721.sol
+++ contracts/mocks/TestERC721.sol
@@ -1,64 +1,240 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-pragma solidity 0.8.7;
+pragma solidity >=0.8.0 <0.9.0;
 
-/// @notice Minimalist NFT minter.
+/// @notice Modern and gas efficient ERC-721 + ERC-20/EIP-2612-like implementation.
 contract TestERC721 {
+    /*///////////////////////////////////////////////////////////////
+                                  EVENTS
+    //////////////////////////////////////////////////////////////*/
+    
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+    
+    event Approval(address indexed owner, address indexed spender, uint256 indexed tokenId);
+    
+    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+    
+    /*///////////////////////////////////////////////////////////////
+                             METADATA STORAGE
+    //////////////////////////////////////////////////////////////*/
+    
+    string public name;
+    
+    string public symbol;
+    
+    /*///////////////////////////////////////////////////////////////
+                             ERC-721 STORAGE
+    //////////////////////////////////////////////////////////////*/
+    
     uint256 public totalSupply;
-    string constant public name = "Test NFT";
-    string constant public symbol = "tNFT";
     
     mapping(address => uint256) public balanceOf;
-    mapping(uint256 => address) public getApproved;
+    
     mapping(uint256 => address) public ownerOf;
+    
     mapping(uint256 => string) public tokenURI;
+    
+    mapping(uint256 => address) public getApproved;
+ 
     mapping(address => mapping(address => bool)) public isApprovedForAll;
     
-    event Approval(address indexed approver, address indexed spender, uint256 indexed tokenId);
-    event ApprovalForAll(address indexed approver, address indexed operator, bool approved);
-    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+    /*///////////////////////////////////////////////////////////////
+                         PERMIT/EIP-2612-LIKE STORAGE
+    //////////////////////////////////////////////////////////////*/
+    
+    bytes32 public constant PERMIT_TYPEHASH =
+        keccak256("Permit(address spender,uint256 tokenId,uint256 nonce,uint256 deadline)");
+        
+    bytes32 public immutable DOMAIN_SEPARATOR;
 
-    function supportsInterface(bytes4 sig) external pure returns (bool) {
-        return (sig == 0x80ac58cd || sig == 0x5b5e139f); // ERC-165
+    mapping(address => uint256) public nonces;
+    
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) {
+        name = _name;
+        symbol = _symbol;
+
+        DOMAIN_SEPARATOR = keccak256(
+            abi.encode(
+                keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+                keccak256(bytes(name)),
+                keccak256(bytes("1")),
+                block.chainid,
+                address(this)
+            )
+        );
+        
+        _mint(msg.sender, 1, "TEST");
+    }
+    
+    /*///////////////////////////////////////////////////////////////
+                              ERC-20-LIKE LOGIC
+    //////////////////////////////////////////////////////////////*/
+    
+    function transfer(address to, uint256 tokenId) external {
+        require(msg.sender == ownerOf[tokenId], "NOT_OWNER");
+        
+        // This is safe because ownership is checked
+        // against decrement, and sum of all user
+        // balances can't exceed type(uint256).max!
+        unchecked {
+            balanceOf[msg.sender]--; 
+        
+            balanceOf[to]++;
+        }
+        
+        delete getApproved[tokenId];
+        
+        ownerOf[tokenId] = to;
+        
+        emit Transfer(msg.sender, to, tokenId); 
+    }
+    
+    /*///////////////////////////////////////////////////////////////
+                              ERC-721 LOGIC
+    //////////////////////////////////////////////////////////////*/
+    
+    function supportsInterface(bytes4 interfaceId) external pure returns (bool supported) {
+        supported = interfaceId == 0x80ac58cd || interfaceId == 0x5b5e139f;
     }
     
     function approve(address spender, uint256 tokenId) external {
         address owner = ownerOf[tokenId];
-        require(msg.sender == owner || isApprovedForAll[owner][msg.sender], "!owner/operator");
+        
+        require(msg.sender == owner || isApprovedForAll[owner][msg.sender], "NOT_APPROVED");
+        
         getApproved[tokenId] = spender;
-        emit Approval(msg.sender, spender, tokenId); 
+        
+        emit Approval(owner, spender, tokenId); 
     }
     
     function setApprovalForAll(address operator, bool approved) external {
         isApprovedForAll[msg.sender][operator] = approved;
+        
         emit ApprovalForAll(msg.sender, operator, approved);
     }
 
-    function mint(uint256 tokenId, string calldata _tokenURI) external { 
-        require(ownerOf[tokenId] == address(0), 'tokenId !unique');
-        totalSupply++;
-        balanceOf[msg.sender]++;
-        ownerOf[tokenId] = msg.sender;
-        tokenURI[tokenId] = _tokenURI;
-        emit Transfer(address(0), msg.sender, tokenId); 
-    }
-
-    function transfer(address to, uint256 tokenId) external {
-        require(msg.sender == ownerOf[tokenId], '!owner');
-        balanceOf[msg.sender]--; 
-        balanceOf[to]++; 
-        getApproved[tokenId] = address(0);
+    function transferFrom(address, address to, uint256 tokenId) public {
+        address owner = ownerOf[tokenId];
+        
+        require(
+            msg.sender == owner 
+            || msg.sender == getApproved[tokenId]
+            || isApprovedForAll[owner][msg.sender], 
+            "NOT_APPROVED"
+        );
+        
+        // This is safe because ownership is checked
+        // against decrement, and sum of all user
+        // balances can't exceed type(uint256).max!
+        unchecked { 
+            balanceOf[owner]--; 
+        
+            balanceOf[to]++;
+        }
+        
+        delete getApproved[tokenId];
+        
         ownerOf[tokenId] = to;
-        emit Transfer(msg.sender, to, tokenId); 
+        
+        emit Transfer(owner, to, tokenId); 
     }
     
-    function transferFrom(address, address to, uint256 tokenId) external {
+    function safetransferFrom(address, address to, uint256 tokenId) external {
+        safetransferFrom(address(0), to, tokenId, "");
+    }
+    
+    function safetransferFrom(address, address to, uint256 tokenId, bytes memory data) public {
+        transferFrom(address(0), to, tokenId); 
+        
+        if (to.code.length != 0) {
+            // selector = `onERC721Received(address,address,uint,bytes)`
+            (, bytes memory returned) = to.staticcall(abi.encodeWithSelector(0x150b7a02,
+                msg.sender, address(0), tokenId, data));
+                
+            bytes4 selector = abi.decode(returned, (bytes4));
+            
+            require(selector == 0x150b7a02, "NOT_ERC721_RECEIVER");
+        }
+    }
+    
+    /*///////////////////////////////////////////////////////////////
+                          PERMIT/EIP-2612-LIKE LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function permit(
+        address spender,
+        uint256 tokenId,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        require(deadline >= block.timestamp, "PERMIT_DEADLINE_EXPIRED");
+        
         address owner = ownerOf[tokenId];
-        require(msg.sender == owner || getApproved[tokenId] == msg.sender || isApprovedForAll[owner][msg.sender], '!owner/spender/operator');
-        balanceOf[owner]--; 
-        balanceOf[to]++; 
-        getApproved[tokenId] = address(0);
+        
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                DOMAIN_SEPARATOR,
+                keccak256(abi.encode(PERMIT_TYPEHASH, spender, tokenId, nonces[owner]++, deadline))
+            )
+        );
+
+        address recoveredAddress = ecrecover(digest, v, r, s);
+        require(recoveredAddress != address(0) 
+                && recoveredAddress == owner
+                || isApprovedForAll[owner][recoveredAddress], 
+                "INVALID_PERMIT_SIGNATURE"
+        );
+        
+        getApproved[tokenId] = spender;
+
+        emit Approval(owner, spender, tokenId);
+    }
+    
+    /*///////////////////////////////////////////////////////////////
+                          INTERNAL UTILS
+    //////////////////////////////////////////////////////////////*/
+    
+    function _mint(address to, uint256 tokenId, string memory _tokenURI) internal { 
+        require(ownerOf[tokenId] == address(0), "ALREADY_MINTED");
+        
+        totalSupply++;
+        
+        // This is safe because the sum of all user
+        // balances can't exceed type(uint256).max!
+        unchecked {
+            balanceOf[to]++;
+        }
+        
         ownerOf[tokenId] = to;
-        emit Transfer(owner, to, tokenId); 
+        
+        tokenURI[tokenId] = _tokenURI;
+        
+        emit Transfer(address(0), to, tokenId); 
+    }
+    
+    function _burn(uint256 tokenId) internal { 
+        address owner = ownerOf[tokenId];
+        
+        require(ownerOf[tokenId] != address(0), "NOT_MINTED");
+        
+        // This is safe because a user won't ever
+        // have a balance larger than totalSupply!
+        unchecked {
+            totalSupply--;
+        
+            balanceOf[owner]--;
+        }
+        
+        delete ownerOf[tokenId];
+        
+        delete tokenURI[tokenId];
+        
+        emit Transfer(owner, address(0), tokenId); 
     }
 }
