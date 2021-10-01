00d49b7858218c8d3ed1698fbd58225718b715df
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-10-01 17:41:08 -0400

    Update LexLockerLite.test.js
    moar test cases, including resolutions

--- test/LexLockerLite.test.js
+++ test/LexLockerLite.test.js
@@ -17,7 +17,7 @@ function getBigNumber(amount, decimals = 18) {
 }
 
 describe("LexLocker", function () {
-  it.only("Should take an ERC20 token deposit and allow release by depositor", async function () {
+  it("Should take an ERC20 token deposit and allow release by depositor", async function () {
     let depositor, receiver, resolver;
     [depositor, receiver, resolver] = await ethers.getSigners();
  
@@ -39,7 +39,7 @@ describe("LexLocker", function () {
     await locker.release(1);
   });
  
-  it("Should take an ERC20 token deposit and forbid release by non-depositor", async function () {
+  it("Should take an ETH deposit and allow release by depositor", async function () {
     let depositor, receiver, resolver;
     [depositor, receiver, resolver] = await ethers.getSigners();
  
@@ -52,13 +52,28 @@ describe("LexLocker", function () {
     await locker.deployed();
  
     const bento = await ethers.getContractAt("IBentoBoxV1", bentoAddress);
-    
-    token.approve(bentoAddress, getBigNumber(10000));
-    token.approve(locker.address, getBigNumber(10000));
-    
+
     await locker.connect(resolver).registerResolver(true, 20);
-    await locker.deposit(receiver.address, resolver.address, token.address, getBigNumber(1000), false, "TEST");
-    await locker.connect(receiver).release(1).should.be.revertedWith("not depositor");
+    await locker.deposit(receiver.address, resolver.address, token.address, getBigNumber(1000), false, "TEST", { value: getBigNumber(1000) });
+    await locker.release(1);
+  });
+
+  it("Should enforce ETH deposit and locker value parity", async function () {
+    let depositor, receiver, resolver;
+    [depositor, receiver, resolver] = await ethers.getSigners();
+ 
+    const Token = await ethers.getContractFactory("TestERC20");
+    const token = await Token.deploy("poc", "poc");
+    await token.deployed();
+ 
+    const Locker = await ethers.getContractFactory("LexLocker");
+    const locker = await Locker.deploy();
+    await locker.deployed();
+ 
+    const bento = await ethers.getContractAt("IBentoBoxV1", bentoAddress);
+
+    await locker.connect(resolver).registerResolver(true, 20);
+    await locker.deposit(receiver.address, resolver.address, token.address, getBigNumber(10000), false, "TEST", { value: getBigNumber(1000) }).should.be.revertedWith("wrong msg.value");
   });
 
   it("Should take an ERC721 NFT deposit and allow release by depositor", async function () {
@@ -66,7 +81,7 @@ describe("LexLocker", function () {
     [depositor, receiver, resolver] = await ethers.getSigners();
  
     const NFT = await ethers.getContractFactory("TestERC721");
-    const nft = await NFT.deploy();
+    const nft = await NFT.deploy("poc", "poc");
     await nft.deployed();
  
     const Locker = await ethers.getContractFactory("LexLocker");
@@ -79,25 +94,204 @@ describe("LexLocker", function () {
     await locker.deposit(receiver.address, resolver.address, nft.address, 1, true, "TEST");
     await locker.release(1);
   });
- 
-  it("Should take an ERC721 NFT deposit and forbid release by non-depositor", async function () {
+
+  it("Should forbid release by non-depositor", async function () {
     let depositor, receiver, resolver;
     [depositor, receiver, resolver] = await ethers.getSigners();
  
-    const NFT = await ethers.getContractFactory("TestERC721");
-    const nft = await NFT.deploy();
-    await nft.deployed();
+    const Token = await ethers.getContractFactory("TestERC20");
+    const token = await Token.deploy("poc", "poc");
+    await token.deployed();
  
     const Locker = await ethers.getContractFactory("LexLocker");
     const locker = await Locker.deploy();
     await locker.deployed();
  
-    await nft.approve(locker.address, 1);
+    const bento = await ethers.getContractAt("IBentoBoxV1", bentoAddress);
+    
+    token.approve(bentoAddress, getBigNumber(10000));
+    token.approve(locker.address, getBigNumber(10000));
     
     await locker.connect(resolver).registerResolver(true, 20);
-    await locker.deposit(receiver.address, resolver.address, nft.address, 1, true, "TEST");
+    await locker.deposit(receiver.address, resolver.address, token.address, getBigNumber(1000), false, "TEST");
     await locker.connect(receiver).release(1).should.be.revertedWith("not depositor");
+    await locker.connect(resolver).release(1).should.be.revertedWith("not depositor");
+  });
+
+  it("Should forbid repeat release", async function () {
+    let depositor, receiver, resolver;
+    [depositor, receiver, resolver] = await ethers.getSigners();
+ 
+    const Token = await ethers.getContractFactory("TestERC20");
+    const token = await Token.deploy("poc", "poc");
+    await token.deployed();
+ 
+    const Locker = await ethers.getContractFactory("LexLocker");
+    const locker = await Locker.deploy();
+    await locker.deployed();
+ 
+    const bento = await ethers.getContractAt("IBentoBoxV1", bentoAddress);
+    
+    token.approve(bentoAddress, getBigNumber(10000));
+    token.approve(locker.address, getBigNumber(10000));
+    
+    await locker.connect(resolver).registerResolver(true, 20);
+    await locker.deposit(receiver.address, resolver.address, token.address, getBigNumber(1000), false, "TEST");
+    await locker.release(1);
+    await locker.release(1).should.be.reverted;
+  });
+
+  it("Should forbid release of nonexistent locker", async function () {
+    const Locker = await ethers.getContractFactory("LexLocker");
+    const locker = await Locker.deploy();
+    await locker.deployed();
+
+    await locker.release(1).should.be.reverted;
+  });
+
+  it("Should allow lock by depositor", async function () {
+    let depositor, receiver, resolver;
+    [depositor, receiver, resolver] = await ethers.getSigners();
+ 
+    const Token = await ethers.getContractFactory("TestERC20");
+    const token = await Token.deploy("poc", "poc");
+    await token.deployed();
+ 
+    const Locker = await ethers.getContractFactory("LexLocker");
+    const locker = await Locker.deploy();
+    await locker.deployed();
+ 
+    const bento = await ethers.getContractAt("IBentoBoxV1", bentoAddress);
+    
+    token.approve(bentoAddress, getBigNumber(10000));
+    token.approve(locker.address, getBigNumber(10000));
+    
+    await locker.connect(resolver).registerResolver(true, 20);
+    await locker.deposit(receiver.address, resolver.address, token.address, getBigNumber(1000), false, "TEST");
+    await locker.lock(1);
+  });
+
+  it("Should allow lock by receiver", async function () {
+    let depositor, receiver, resolver;
+    [depositor, receiver, resolver] = await ethers.getSigners();
+ 
+    const Token = await ethers.getContractFactory("TestERC20");
+    const token = await Token.deploy("poc", "poc");
+    await token.deployed();
+ 
+    const Locker = await ethers.getContractFactory("LexLocker");
+    const locker = await Locker.deploy();
+    await locker.deployed();
+ 
+    const bento = await ethers.getContractAt("IBentoBoxV1", bentoAddress);
+    
+    token.approve(bentoAddress, getBigNumber(10000));
+    token.approve(locker.address, getBigNumber(10000));
+    
+    await locker.connect(resolver).registerResolver(true, 20);
+    await locker.deposit(receiver.address, resolver.address, token.address, getBigNumber(1000), false, "TEST");
+    await locker.connect(receiver).lock(1);
+  });
+
+  it("Should forbid lock by non-party", async function () {
+    let depositor, receiver, resolver;
+    [depositor, receiver, resolver] = await ethers.getSigners();
+ 
+    const Token = await ethers.getContractFactory("TestERC20");
+    const token = await Token.deploy("poc", "poc");
+    await token.deployed();
+ 
+    const Locker = await ethers.getContractFactory("LexLocker");
+    const locker = await Locker.deploy();
+    await locker.deployed();
+ 
+    const bento = await ethers.getContractAt("IBentoBoxV1", bentoAddress);
+    
+    token.approve(bentoAddress, getBigNumber(10000));
+    token.approve(locker.address, getBigNumber(10000));
+    
+    await locker.connect(resolver).registerResolver(true, 20);
+    await locker.deposit(receiver.address, resolver.address, token.address, getBigNumber(1000), false, "TEST");
+    await locker.connect(resolver).lock(1).should.be.revertedWith("not locker party");
+  });
+ 
+  it("Should allow resolution by resolver", async function () {
+    let depositor, receiver, resolver;
+    [depositor, receiver, resolver] = await ethers.getSigners();
+ 
+    const Token = await ethers.getContractFactory("TestERC20");
+    const token = await Token.deploy("poc", "poc");
+    await token.deployed();
+ 
+    const Locker = await ethers.getContractFactory("LexLocker");
+    const locker = await Locker.deploy();
+    await locker.deployed();
+ 
+    const bento = await ethers.getContractAt("IBentoBoxV1", bentoAddress);
+    
+    token.approve(bentoAddress, getBigNumber(10000));
+    token.approve(locker.address, getBigNumber(10000));
+    
+    await locker.connect(resolver).registerResolver(true, 20);
+    await locker.deposit(receiver.address, resolver.address, token.address, getBigNumber(1000), false, "TEST");
+    await locker.lock(1);
+    
+    const resolutionAmount = getBigNumber(1000).div(2);
+
+    await locker.connect(resolver).resolve(1, resolutionAmount, resolutionAmount, "TEST");
+  });
+
+  it("Should forbid resolution by non-resolver", async function () {
+    let depositor, receiver, resolver;
+    [depositor, receiver, resolver] = await ethers.getSigners();
+ 
+    const Token = await ethers.getContractFactory("TestERC20");
+    const token = await Token.deploy("poc", "poc");
+    await token.deployed();
+ 
+    const Locker = await ethers.getContractFactory("LexLocker");
+    const locker = await Locker.deploy();
+    await locker.deployed();
+ 
+    const bento = await ethers.getContractAt("IBentoBoxV1", bentoAddress);
+    
+    token.approve(bentoAddress, getBigNumber(10000));
+    token.approve(locker.address, getBigNumber(10000));
+    
+    await locker.connect(resolver).registerResolver(true, 20);
+    await locker.deposit(receiver.address, resolver.address, token.address, getBigNumber(1000), false, "TEST");
+    await locker.lock(1);
+    
+    const resolutionAmount = getBigNumber(1000).div(2);
+
+    await locker.connect(depositor).resolve(1, resolutionAmount, resolutionAmount, "TEST").should.be.revertedWith("not resolver");
+    await locker.connect(receiver).resolve(1, resolutionAmount, resolutionAmount, "TEST").should.be.revertedWith("not resolver");
+  });
+  
+  it("Should forbid repeat resolution", async function () {
+    let depositor, receiver, resolver;
+    [depositor, receiver, resolver] = await ethers.getSigners();
+ 
+    const Token = await ethers.getContractFactory("TestERC20");
+    const token = await Token.deploy("poc", "poc");
+    await token.deployed();
+ 
+    const Locker = await ethers.getContractFactory("LexLocker");
+    const locker = await Locker.deploy();
+    await locker.deployed();
+ 
+    const bento = await ethers.getContractAt("IBentoBoxV1", bentoAddress);
+    
+    token.approve(bentoAddress, getBigNumber(10000));
+    token.approve(locker.address, getBigNumber(10000));
+    
+    await locker.connect(resolver).registerResolver(true, 20);
+    await locker.deposit(receiver.address, resolver.address, token.address, getBigNumber(1000), false, "TEST");
+    await locker.lock(1);
+    
+    const resolutionAmount = getBigNumber(1000).div(2);
+
+    await locker.connect(resolver).resolve(1, resolutionAmount, resolutionAmount, "TEST");
+    await locker.connect(resolver).resolve(1, resolutionAmount, resolutionAmount, "TEST").should.be.reverted;
   });
 });
- 
-
