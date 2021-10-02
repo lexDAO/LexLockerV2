4c4c9d447a1fb346247fd500ab205da0d3efb97e
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-10-02 17:25:36 -0400

    Update hardhat.config.js
    bump solidity

--- hardhat.config.js
+++ hardhat.config.js
@@ -58,7 +58,7 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.8.7",
+        version: "0.8.9",
         settings: {
           optimizer: {
             enabled: true,
